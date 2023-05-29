0167cb2fc72cb2bc3bcdb6730b94d5f938d8044f
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2023-05-29 22:42:25 +0200

    Crea controller e collega in web.php

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
+
+Route::middleware('auth')
+    ->namespace('Admin')
+    ->prefix('admin')
+    ->name('admin.')
+    ->group(function () {
+        Route::get('/', 'HomeController@index')->name('index');
+    });
+
+Route::get('{any?}', function () {
+    return view('guest.home');
+})->where("any", ".\*");
 
 Route::get('/home', 'HomeController@index')->name('home');
