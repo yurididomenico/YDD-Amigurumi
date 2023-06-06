b20dc4cbcd1fcb37aaec5e37e14728bd22820544
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2023-06-06 18:22:37 +0200

    Relazione sizes_puppets 1-n

--- app/Puppet.php
+++ app/Puppet.php
@@ -8,7 +8,13 @@ class Puppet extends Model
 {
     protected $table = 'puppets'; // Specifica il nome della tabella corrispondente
 
-    protected $fillable = ['title', 'body', 'cover']; // Definisci i campi che possono essere assegnati in modo massivo
+    protected $fillable = ['title', 'body', 'cover', 'size_id']; // Definisci i campi che possono essere assegnati in modo massivo
+
+    public function size()
+    {
+        // Funzione di relazione
+        return $this->belongsTo('App\Size');  //Il singolo puppet avrà una sola taglia associata
+    }
 
     // Eventuali altre relazioni, accessori o metodi del modello possono essere definiti qui
 }
--- app/Size.php
+++ app/Size.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Size extends Model
 {
-    //
+    public function puppets()
+    {
+        // Funzione di relazione
+
+        return $this->hasMany('App\Puppet'); // La taglia ha tanti puppets associati -->
+    }
 }
